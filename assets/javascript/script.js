0472cbfacced735536117ccdd6093ca6894a33ac
Mohammad <rto1680@gmail.com>
2018-06-26 12:46:18 +0430

    add project into github repo

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+/*****************
+*
+*    Theme Name: 'Loader'
+*    Theme URL: 'projects.mohammadrahmani.com/loader/'
+*    Author: Mohammad Rahmani
+*    Author URL: http://mohammadrahmani.com
+*
+*/
+
+$(window).on('load', function() {
+
+    // for hide loader
+
+    //$('.loader').addClass('loaded');
+});
